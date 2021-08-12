0e73d7b9be005f4e7873cf604556d59e3119f1d8
Gage Miller <miller2590@gmail.com>
2021-08-11 20:04:15 -0500

    built hamburger menu inside media query

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
+// Toggles the hamburger menu on
 
+const hamburger = document.querySelector(".hamburger");
+const linkMenu = document.querySelector(".links");
+
+hamburger.addEventListener("click", mobileNav);
+
+function mobileNav() {
+    hamburger.classList.toggle("active");
+    linkMenu.classList.toggle("active");
+}
+
+// Toggles the hamburger menu off
+
+const navLink = document.querySelectorAll(".nav-link");
+
+navLink.forEach(n => n.addEventListener("click", closeMenu));
+
+function closeMenu() {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+}
